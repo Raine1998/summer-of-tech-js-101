6f8517475bbf504e19add6b074499c9926b1c19c
Raine <legaspi.raine@yahoo.com>
2021-05-04 21:02:27 +1200

    add button flipCoin

--- src/script.js
+++ src/script.js
@@ -1,5 +1,12 @@
 hello();
 
+let button = document.getElementById('flipCoin');
+button.addEventListener('click', function () {
+       alert('FLIP');
+   });
+
+
+
 function hello() {
        alert('Hello Ashley');
    
